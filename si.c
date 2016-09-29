8c9b22f15a0b6d11c4f2ab927a9e71fc73563a4d
DiegoCasillasM <zenet_enp2@hotmail.com>
2016-09-29 08:19:38 -0500

    Ejercicio "si"
    asdfghjklñ

---
+++ si.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+INICIO
+   a:=5:ENTERO
+   SI a<>5 ENTONCES
+      ESCRIBIR "a es diferente de 5"
+   FIN SI
+FIN
+
+*/
+
+int main()
+{
+   int a=5;
+    if (a!=5)
+    {
+       printf("a es diferente de 5");
+    }
+    return 0;
+}
