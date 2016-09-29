a8b5739df233aaee76b7e40dfbe258f9e9abf82e
DiegoCasillasM <zenet_enp2@hotmail.com>
2016-09-29 08:18:39 -0500

    Ejercicio SI ENTONCES
    BLA BLA BLA

---
+++ sientonces.c
@@ -0,0 +1,29 @@
+/*
+INICIO
+    a:=6:ENTERO
+    b:=3:ENTERO
+    SI a=b ENTONCES
+        ESCRIBIR "a es igual a b"
+    FIN SI
+    DE LO CONTRARIO
+        ESCRIBIR "a es diferente de b"
+    FIN DE LO CONTRARIO
+FIN
+*/
+
+#include <stdio.h>
+
+int main ()
+{
+ int a=6,b=3;
+ if (a==b)
+ {
+    printf("a es igual a b");
+ }
+ else
+ {
+    printf("a es diferente de b");
+ }
+ return 0;
+
+}
